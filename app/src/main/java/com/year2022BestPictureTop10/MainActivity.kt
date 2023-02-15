13d6ce59af3dd33009b09b8d678d21407263bf8f
Rayched <ny2401@naver.com>
2023-02-15 15:59:15 +0900

    git repository connect test

---
+++ app/src/main/java/com/year2022BestPictureTop10/MainActivity.kt
@@ -0,0 +1,13 @@
+//Minimum SDK => Mashmellow
+
+package com.year2022BestPictureTop10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
